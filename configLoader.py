590e0a587bc0d0682e14cf357277160a5ac115f1
Alex O'Neill <alex.b.oneill@outlook.com>
2021-09-08 00:11:52 -0400

    Added config file system Added config file testing system Added drink file testing system

---
+++ configLoader.py
@@ -0,0 +1,37 @@
+def validateConfig(config):
+    #Make sure server_roles key exists
+    assert "server_roles" in config
+    
+    #Make sure all server roles have a pronoun and can_serve value
+    for key in config["server_roles"]:
+        
+        value = config["server_roles"][key]
+
+        #Check that there's a pronoun
+        assert "pronoun" in value
+        assert type(value["pronoun"]) is str
+
+        #Check that there's a can_serve key
+        assert "can_serve" in value
+        assert type(value["can_serve"]) is bool
+
+    #Make sure there's minor roles
+    assert "minor_roles" in config
+    assert type(config["minor_roles"]) is list
+
+    #Make sure we specify a menu_page_size
+    assert "menu_page_size" in config
+    assert type(config["menu_page_size"]) is int
+
+    #Make sure we have a drinkfile specified
+    assert "drinkfile" in config
+    assert type(config["drinkfile"]) is str
+
+    #Make sure we have a token file specified
+    assert "tokenfile" in config
+    assert type(config["tokenfile"]) is str
+
+    
+
+
+
